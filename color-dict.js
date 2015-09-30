c4103032905e4d999eda006e3862bfcc4c1adf5e
John Sta <johnsta2oo@gmail.com>
2015-09-30 18:30:21 -0500

    Update color-dict.js

--- color-dict.js
+++ color-dict.js
@@ -2,8 +2,8 @@ function pebble_to_hex(pColor){
   alert("pColor: " + pColor);
   for (var key in color_picker_colors){
     alert("key: " + key);
-    alert("binary: " + color_picker_colors.key.binary);
-    if (Number(color_picker_colors.key.binary) == pColor){
+    alert("binary: " + color_picker_colors[key].binary);
+    if (Number(color_picker_colors[key].binary) == pColor){
       return key.replace('#', '0x');
     }
   }
