4c093bd38bb293c8e71835f0202597b33a989ce1
John Sta <johnsta2oo@gmail.com>
2015-10-09 16:03:42 -0500

    Update SaveRestore.js

--- tools/SaveRestore.js
+++ tools/SaveRestore.js
@@ -8,6 +8,29 @@ function RestoreIt(id){
 	ImportValues(localStorage.getItem("key" + id + "_query"));
 }
 
+var loaded = false;
 function LoadSaved(uiID){
+	if (!loaded){
+		
+	var lStg = localStorage.getItem("list");
+	var vars = lStg.split(",");
 	
+	var target = document.getElementyById(uiID);
+	//var aa = {};
+	
+	for (var i=0;i<vars.length;i++) {
+		
+		var sName = "key" + var[i] + "_name";
+		var sQuery = "key" + var[i] + "_query";
+	    //aa[sName] = sQuery;
+	    
+	    var entry = document.createElement('li');
+	    entry.appendChild(document.createTextNode(sName));
+	    entry.setElement("query", sQuery);
+	    target.appendDhile(entry);
+
+	  }		
+		
+		loaded = true;
+	}
 }
