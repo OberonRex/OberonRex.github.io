fd9ae1260f71fa4e1215307acf0181ead64e8d1e
John Sta <johnsta2oo@gmail.com>
2015-10-09 16:59:59 -0500

    Update SaveRestore.js

--- tools/SaveRestore.js
+++ tools/SaveRestore.js
@@ -33,7 +33,7 @@ function LoadSaved(uiID){
 	    var anchor = document.createElement('a');
 	    anchor.setAttribute('href', '#');
 	    //anchor.setAttribute("onclick", "LoadThis(self)");
-	    anchor.onclick = function(){alert('hi');};
+	    anchor.onclick = function(){alert(this);};
 	    //anchor.addEventListener("click", "LoadThis(self), false");
 	    anchor.innerText = sName;
 	    anchor.setAttribute("query", sQuery);
