5c07a66f9a1118c88b1d6ef7833609fcec45341b
John Sta <johnsta2oo@gmail.com>
2015-10-09 15:48:37 -0500

    Update SaveRestore.js

--- tools/SaveRestore.js
+++ tools/SaveRestore.js
@@ -1,1 +1,9 @@
-test
+function SaveIt(id){
+	localStorage.setItem("key" + id + "_name", "dummy");
+	localStorage.setItem("key" + id + "_query", GenerateQueryString());
+}
+
+function RestoreIt(id){
+	ImportValues(localStorage.getItem("key" + id + "_query"));
+}
+
