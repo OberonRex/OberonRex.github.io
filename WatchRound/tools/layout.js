c47f36feddd4cb04af884c1368c7cb3a39e5711b
John Sta <johnsta2oo@gmail.com>
2015-12-11 12:55:21 -0600

    Update layout.js

--- WatchRound/tools/layout.js
+++ WatchRound/tools/layout.js
@@ -18,14 +18,18 @@ function jps_toggleShowList(wrapper){
 //}
 
 function wireUpFramework(){
+	var boxWidth = screen.width;
+	if (boxWidth > 480) boxWidth = 360; else boxWidth = Math.min(screen.width, 480);
+	boxWidth -= 20;
+	var rule = ".jps_stdBox {width:" + boxWidth + "px;clear:both;}"
+	document.styleSheets[0].insertRule(rule, 1);
+
 	$('div#jps_MasterDiv').on('click', '.jps_AddButton', function() {jps_AddOne(this.parentElement)});
 	$('div#jps_MasterDiv').on('click', '.jps_ListHeader', function(){jps_toggleShowList(this.parentElement);});
 	$('div#jps_MasterDiv').on('click', '.jps_RemoveButton', function() {jps_RemoveOne(this.parentElement)});
 	
-	//<input class="jps_2ndColumn" type="text" onblur="jps_MakeRO(this)" onfocus="jps_MakeRW(this)" readonly=true>
 	$('div#jps_MasterDiv').on('focus', '.jps_Text', function(){this.removeAttribute('readonly')});
 	$('div#jps_MasterDiv').on('blur', '.jps_Text', function(){this.setAttribute('readonly', true)});
-
 }
 
 function jps_AddOne(pElem){
