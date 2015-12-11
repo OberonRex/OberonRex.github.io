1ac0f76a3feabac7acc72d13edfcfdd3fe2fbe40
John Sta <johnsta2oo@gmail.com>
2015-12-11 13:04:10 -0600

    Update layout.js

--- WatchRound/tools/layout.js
+++ WatchRound/tools/layout.js
@@ -30,6 +30,6 @@ function jps_AddOne(pElem){
 	pElem.appendChild(clone);
 	
 	clone.classList.remove('jps_Template');
-	clone.classList.add('jps_ListItem');
-	clone.classList.add('jps_ListWrapper');
+	//clone.classList.add('jps_ListItem');
+	//clone.classList.add('jps_ListWrapper');
 }
