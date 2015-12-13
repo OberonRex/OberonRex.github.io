0573e7df86991c7b374d12e7cbe01d74c6c92e11
John Sta <johnsta2oo@gmail.com>
2015-12-13 16:25:07 -0600

    Update properties.js

--- WatchRound/tools/properties.js
+++ WatchRound/tools/properties.js
@@ -60,7 +60,7 @@ function ImportValues(stg){
 	    
 	    // can we find an element with the id?
 	    var elem = document.getElementById(pair[0]);
-	    if (elem != NULL){
+	    if (elem != null){
 	    	if ((elem.tagName == "div") && elem.classList.contains('jps_ListWrapper')){
 	    		var cnt = parseInt(pair[1]);
 	    		for(var n = 0; n < cnt; n++) jps_AddOne(elem, false);
