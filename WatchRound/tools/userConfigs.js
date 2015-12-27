c30d9fe3234391a30ede38d1a7298dcc2797dc17
John Sta <johnsta2oo@gmail.com>
2015-12-26 20:35:58 -0600

    Update userConfigs.js

--- WatchRound/tools/userConfigs.js
+++ WatchRound/tools/userConfigs.js
@@ -1,18 +1,44 @@
 /*
 * For everything to do with user configurations
 */
-function findInput(parentElem){
-  return parentElem.getElementsByClassName('jps_Text')[0];
+
+function findConfig(elem){
+	while (!elem.classSet.contains('jps_Config')) elem = elem.parentElem;
+	return elem.;
+}
+
+function findInput(configElem){
+  return configElem.getElementsByClassName('jps_ConfigName')[0];
 }
 
 function jps_AddUserConfig(parentElem){
-	var e = findInput(jps_AddOne(pElem, true));
+	var newConfig = jps_AddOne(pElem, true);
+	var e = findInput(newConfig);
+	e.value = "New";
 	var l = e.value.length;
 	e.setSelectionRange(l, l);
 	e.value = e.value;
 	e.focus();
+	CaptureConfig(newConfig);
+}
+
+function CaptureConfig(configElem){
+	var propStg = Encode(jps_PropertyString());
+	configElem.setAttribute('properties', propStg);
 }
 
 function user_SaveNewName(elem){
   
+}
+
+user_Apply(configElem){
+	
+}
+
+user_Update(configElem){
+	
+}
+
+user_Delete(configElem){
+	alert("Delete");
 }
