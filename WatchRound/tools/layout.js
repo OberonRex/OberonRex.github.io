1c2ca8a4f67679c83547063b8b62fd40f74a79a8
John Sta <johnsta2oo@gmail.com>
2016-01-10 12:40:27 -0600

    Update layout.js

--- WatchRound/tools/layout.js
+++ WatchRound/tools/layout.js
@@ -59,7 +59,7 @@ function setupCustomElements(){
 		chkBox.type = 'checkbox';
 		var idInp = this.id + "_Inp";
 		chkBox.id = idInp;
-		chkBox.onclick = "alert();"
+		chkBox.onclick = function(){alert();};
 		
 		var label = document.createElement('label');
 		label.htmlFor = idInp;
